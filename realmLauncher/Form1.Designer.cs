bf0d9cf625caeac8ada5a38138e471f029891d5f
Jedi <Jedi@VADER>
2016-04-23 02:10:01 -0700

    Add project files.

---
+++ realmLauncher/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace realmLauncher
+{
+    partial class realmLauncher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.chkAppend = new System.Windows.Forms.CheckBox();
+            this.lblSelectedRealm = new System.Windows.Forms.Label();
+            this.btnRealmSave = new System.Windows.Forms.Button();
+            this.txtImportURL = new System.Windows.Forms.TextBox();
+            this.btnImportRealm = new System.Windows.Forms.Button();
+            this.btnRemoveRealm = new System.Windows.Forms.Button();
+            this.btnLaunchRealm = new System.Windows.Forms.Button();
+            this.realmView = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnSaveClients = new System.Windows.Forms.Button();
+            this.btnRemoveVersion = new System.Windows.Forms.Button();
+            this.clientView = new System.Windows.Forms.DataGridView();
+            this.lblSelectedClient = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.realmView)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.clientView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(710, 390);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.chkAppend);
+            this.tabPage1.Controls.Add(this.lblSelectedRealm);
+            this.tabPage1.Controls.Add(this.btnRealmSave);
+            this.tabPage1.Controls.Add(this.txtImportURL);
+            this.tabPage1.Controls.Add(this.btnImportRealm);
+            this.tabPage1.Controls.Add(this.btnRemoveRealm);
+            this.tabPage1.Controls.Add(this.btnLaunchRealm);
+            this.tabPage1.Controls.Add(this.realmView);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(702, 364);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Realms";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // chkAppend
+            // 
+            this.chkAppend.AutoSize = true;
+            this.chkAppend.Location = new System.Drawing.Point(558, 308);
+            this.chkAppend.Name = "chkAppend";
+            this.chkAppend.Size = new System.Drawing.Size(142, 17);
+            this.chkAppend.TabIndex = 7;
+            this.chkAppend.Text = "Do Not Replace Existing";
+            this.chkAppend.UseVisualStyleBackColor = true;
+            // 
+            // lblSelectedRealm
+            // 
+            this.lblSelectedRealm.AutoSize = true;
+            this.lblSelectedRealm.Location = new System.Drawing.Point(154, 340);
+            this.lblSelectedRealm.Name = "lblSelectedRealm";
+            this.lblSelectedRealm.Size = new System.Drawing.Size(76, 13);
+            this.lblSelectedRealm.TabIndex = 6;
+            this.lblSelectedRealm.Text = "(Select Realm)";
+            // 
+            // btnRealmSave
+            // 
+            this.btnRealmSave.Location = new System.Drawing.Point(559, 335);
+            this.btnRealmSave.Name = "btnRealmSave";
+            this.btnRealmSave.Size = new System.Drawing.Size(141, 23);
+            this.btnRealmSave.TabIndex = 5;
+            this.btnRealmSave.Text = "Save Changes";
+            this.btnRealmSave.UseVisualStyleBackColor = true;
+            this.btnRealmSave.Click += new System.EventHandler(this.btnRealmSave_Click);
+            // 
+            // txtImportURL
+            // 
+            this.txtImportURL.Location = new System.Drawing.Point(6, 306);
+            this.txtImportURL.Name = "txtImportURL";
+            this.txtImportURL.Size = new System.Drawing.Size(404, 20);
+            this.txtImportURL.TabIndex = 4;
+            // 
+            // btnImportRealm
+            // 
+            this.btnImportRealm.Location = new System.Drawing.Point(411, 305);
+            this.btnImportRealm.Name = "btnImportRealm";
+            this.btnImportRealm.Size = new System.Drawing.Size(142, 23);
+            this.btnImportRealm.TabIndex = 3;
+            this.btnImportRealm.Text = "Import Realms";
+            this.btnImportRealm.UseVisualStyleBackColor = true;
+            this.btnImportRealm.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnRemoveRealm
+            // 
+            this.btnRemoveRealm.Location = new System.Drawing.Point(6, 335);
+            this.btnRemoveRealm.Name = "btnRemoveRealm";
+            this.btnRemoveRealm.Size = new System.Drawing.Size(142, 23);
+            this.btnRemoveRealm.TabIndex = 2;
+            this.btnRemoveRealm.Text = "Remove Selected Realm";
+            this.btnRemoveRealm.UseVisualStyleBackColor = true;
+            this.btnRemoveRealm.Click += new System.EventHandler(this.btnRemoveRealm_Click);
+            // 
+            // btnLaunchRealm
+            // 
+            this.btnLaunchRealm.Location = new System.Drawing.Point(411, 335);
+            this.btnLaunchRealm.Name = "btnLaunchRealm";
+            this.btnLaunchRealm.Size = new System.Drawing.Size(142, 23);
+            this.btnLaunchRealm.TabIndex = 1;
+            this.btnLaunchRealm.Text = "Launch Selected Realm";
+            this.btnLaunchRealm.UseVisualStyleBackColor = true;
+            this.btnLaunchRealm.Click += new System.EventHandler(this.btnLaunchRealm_Click);
+            // 
+            // realmView
+            // 
+            this.realmView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.realmView.Location = new System.Drawing.Point(0, 0);
+            this.realmView.Name = "realmView";
+            this.realmView.Size = new System.Drawing.Size(700, 300);
+            this.realmView.TabIndex = 0;
+            this.realmView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.realmView_CellContentClick);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lblSelectedClient);
+            this.tabPage2.Controls.Add(this.btnSaveClients);
+            this.tabPage2.Controls.Add(this.btnRemoveVersion);
+            this.tabPage2.Controls.Add(this.clientView);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(702, 364);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Clients";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnSaveClients
+            // 
+            this.btnSaveClients.Location = new System.Drawing.Point(559, 335);
+            this.btnSaveClients.Name = "btnSaveClients";
+            this.btnSaveClients.Size = new System.Drawing.Size(141, 23);
+            this.btnSaveClients.TabIndex = 7;
+            this.btnSaveClients.Text = "Save Changes";
+            this.btnSaveClients.UseVisualStyleBackColor = true;
+            this.btnSaveClients.Click += new System.EventHandler(this.btnSaveClients_Click);
+            // 
+            // btnRemoveVersion
+            // 
+            this.btnRemoveVersion.Location = new System.Drawing.Point(6, 335);
+            this.btnRemoveVersion.Name = "btnRemoveVersion";
+            this.btnRemoveVersion.Size = new System.Drawing.Size(140, 23);
+            this.btnRemoveVersion.TabIndex = 6;
+            this.btnRemoveVersion.Text = "Remove Client Version";
+            this.btnRemoveVersion.UseVisualStyleBackColor = true;
+            this.btnRemoveVersion.Click += new System.EventHandler(this.btnRemoveVersion_Click);
+            // 
+            // clientView
+            // 
+            this.clientView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.clientView.Location = new System.Drawing.Point(0, 0);
+            this.clientView.Name = "clientView";
+            this.clientView.Size = new System.Drawing.Size(700, 300);
+            this.clientView.TabIndex = 0;
+            this.clientView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.clientView_CellContentClick);
+            // 
+            // lblSelectedClient
+            // 
+            this.lblSelectedClient.AutoSize = true;
+            this.lblSelectedClient.Location = new System.Drawing.Point(154, 340);
+            this.lblSelectedClient.Name = "lblSelectedClient";
+            this.lblSelectedClient.Size = new System.Drawing.Size(72, 13);
+            this.lblSelectedClient.TabIndex = 8;
+            this.lblSelectedClient.Text = "(Select Client)";
+            // 
+            // realmLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 425);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "realmLauncher";
+            this.Text = "Realm Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.realmView)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.clientView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView realmView;
+        private System.Windows.Forms.DataGridView clientView;
+        private System.Windows.Forms.TextBox txtImportURL;
+        private System.Windows.Forms.Button btnImportRealm;
+        private System.Windows.Forms.Button btnRemoveRealm;
+        private System.Windows.Forms.Button btnLaunchRealm;
+        private System.Windows.Forms.Button btnRemoveVersion;
+        private System.Windows.Forms.Button btnRealmSave;
+        private System.Windows.Forms.Button btnSaveClients;
+        private System.Windows.Forms.Label lblSelectedRealm;
+        private System.Windows.Forms.CheckBox chkAppend;
+        private System.Windows.Forms.Label lblSelectedClient;
+    }
+}
+
---
+++ realmLauncher/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Data;
+using System.Xml.Serialization;
+using System.IO;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Net;
+using System.Diagnostics;
+
+namespace realmLauncher
+{
+    public partial class realmLauncher : Form
+    {
+        DataTable realmTable { get; set; }
+        DataTable clientTable { get; set; }
+        string importURL = ConfigurationManager.AppSettings["importURL"].ToString();
+        string clientConfigFile = ConfigurationManager.AppSettings["clientConfigFile"].ToString();
+        string realmConfigFile = ConfigurationManager.AppSettings["realmConfigFile"].ToString();
+        string importFile { get; set; }
+        int realmRowSelected { get; set; }
+        int clientRowSelected { get; set; }
+
+        public realmLauncher()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            realmTable = makeRealmTable();
+            clientTable = makeClientTable();
+            loadRealms(realmConfigFile, true);
+            loadClients(clientConfigFile);
+            txtImportURL.Text = importURL;
+            chkAppend.Checked = true;
+            drawGrids();
+        }
+
+        private void loadRealms(string xmlFile, bool isInternal)
+        {
+            try
+            {
+                XmlSerializer xread = new XmlSerializer(typeof(config));
+                using (FileStream fs = new FileStream(Path.Combine(Directory.GetCurrentDirectory(), xmlFile), FileMode.Open))
+                {
+                    config cfg = xread.Deserialize(fs) as config;
+                    if (!chkAppend.Checked)
+                    {
+                        realmTable.Clear();
+                        realmTable = makeRealmTable();
+                    }
+                    foreach (var r in cfg.realms)
+                    {
+                        DataRow realm = realmTable.NewRow();
+                        realm["id"] = (realmTable.Rows.Count + 1).ToString();
+                        realm["name"] = r.name;
+                        realm["realmlist"] = r.realmlist;
+                        realm["patchlist"] = r.patchlist;
+                        realm["clientversion"] = r.clientversion;
+                        realm["website"] = r.website;
+                        realmTable.Rows.Add(realm);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //i should not leave this blank
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void loadClients(string xmlFile)
+        {
+            try
+            {
+                XmlSerializer xread = new XmlSerializer(typeof(config));
+                using (FileStream fs = new FileStream(Path.Combine(Directory.GetCurrentDirectory(), xmlFile), FileMode.Open))
+                {
+                    config cfg = xread.Deserialize(fs) as config;
+                    clientTable = makeClientTable();
+                    foreach (var c in cfg.clients)
+                    {
+                        DataRow client = clientTable.NewRow();
+                        client["id"] = c.id;
+                        client["clientversion"] = c.clientversion;
+                        client["clientpath"] = c.clientpath;
+                        clientTable.Rows.Add(client);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //i should not leave this blank
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void drawGrids()
+        {
+            reNumber(realmTable);
+            reNumber(clientTable);
+            lblSelectedRealm.Text = "(Select Realm)";
+            chkAppend.Enabled = true;
+            btnLaunchRealm.Enabled = false;
+            btnRemoveRealm.Enabled = false;
+            btnRemoveVersion.Enabled = false;
+            Padding p = new Padding(0, 0, 0, 0);
+            realmView.DataSource = realmTable;
+            clientView.DataSource = clientTable;
+            realmView.RowHeadersVisible = false;
+            realmView.RowTemplate.DefaultCellStyle.Padding = p;
+            clientView.RowHeadersVisible = false;
+            clientView.RowTemplate.DefaultCellStyle.Padding = p;
+            realmView.Columns[0].Width = 25;
+            realmView.Columns[1].Width = 90;
+            realmView.Columns[2].Width = 40;
+            realmView.Columns[3].Width = 180;
+            realmView.Columns[4].Width = 180;
+            realmView.Columns[5].Width = 180;
+            realmView.Columns[0].HeaderText = "Id";
+            realmView.Columns[1].HeaderText = "Name";
+            realmView.Columns[2].HeaderText = "Patch";
+            realmView.Columns[3].HeaderText = "WWW";
+            realmView.Columns[4].HeaderText = "Realmlist";
+            realmView.Columns[5].HeaderText = "Patchlist (optional)";
+            clientView.Columns[0].Width = 25;
+            clientView.Columns[1].Width = 40;
+            clientView.Columns[2].Width = 630;
+            clientView.Columns[0].HeaderText = "Id";
+            clientView.Columns[1].HeaderText = "Patch";
+            clientView.Columns[2].HeaderText = "Path";
+        }
+
+        private void reNumber(DataTable dt)
+        {
+            int i=1;
+            foreach(DataRow r in dt.Rows)
+            {
+                r[0] = i++;
+            }
+        }
+
+        private DataTable makeClientTable()
+        {
+            DataTable client = new DataTable();
+            client.TableName = "client";
+            client.Columns.Add("id");
+            client.Columns.Add("clientversion");
+            client.Columns.Add("clientpath");
+            return client;
+        }
+
+        private DataTable makeRealmTable()
+        {
+            DataTable realm = new DataTable();
+            realm.TableName = "realm";
+            realm.Columns.Add("id");
+            realm.Columns.Add("name");
+            realm.Columns.Add("clientversion");
+            realm.Columns.Add("website");
+            realm.Columns.Add("realmlist");
+            realm.Columns.Add("patchlist");
+            return realm;
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                importURL = txtImportURL.Text.ToString();
+                getFileFromGoogle();
+                loadRealms(importFile,false);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            drawGrids();
+        }
+
+        private void getFileFromGoogle()
+        {
+            try {
+                importFile = DateTime.Now.ToString("yyyyMMddTHHmmss") + ".xml";
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile(importURL, importFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void writeXML(string configFile, DataTable ds, string which)
+        {
+            try
+            {
+                //because changing the parent node is impossible without using a dataset
+                DataSet pn = new DataSet();
+                pn.DataSetName = which;
+                pn.Tables.Add(ds);
+                File.Move(configFile, "backup_" + DateTime.Now.ToString("yyyyMMddTHHmmss") + ".xml");
+                using (FileStream fs = File.Open(configFile, FileMode.Create))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        sw.WriteLine("<config>");//and this is ugly too
+                        ds.WriteXml(sw);
+                        sw.WriteLine("</config>");//and this is ugly too
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnRemoveRealm_Click(object sender, EventArgs e)
+        {
+            DataRow selected = realmTable.Rows[realmRowSelected];
+            DialogResult doIt = MessageBox.Show("Really remove realm " + selected[1].ToString(),"Really?",MessageBoxButtons.YesNo);
+            if (doIt==DialogResult.Yes)
+            {
+                realmTable.Rows.RemoveAt(realmRowSelected);
+            }
+            drawGrids();
+        }
+
+        private void btnRemoveVersion_Click(object sender, EventArgs e)
+        {
+            DataRow selected = clientTable.Rows[clientRowSelected];
+            DialogResult doIt = MessageBox.Show("Really remove client " + selected[1].ToString(), "Really?", MessageBoxButtons.YesNo);
+            if (doIt == DialogResult.Yes)
+            {
+                clientTable.Rows.RemoveAt(clientRowSelected);
+            }
+            drawGrids();
+        }
+
+        private void realmView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            realmRowSelected = e.RowIndex;
+            lblSelectedRealm.Text = "Selected ID: " + realmView.Rows[e.RowIndex].Cells[0].Value.ToString();
+            btnRemoveRealm.Enabled = true;
+            btnLaunchRealm.Enabled = true;
+        }
+
+        private void clientView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            clientRowSelected = e.RowIndex;
+            lblSelectedClient.Text = "Selected ID: " + clientView.Rows[e.RowIndex].Cells[0].Value.ToString();
+            btnRemoveVersion.Enabled = true;
+        }
+
+        private void btnSaveClients_Click(object sender, EventArgs e)
+        {
+            writeXML(clientConfigFile,clientTable,"clients");
+        }
+
+        private void btnRealmSave_Click(object sender, EventArgs e)
+        {
+            writeXML(realmConfigFile,realmTable,"realms");
+        }
+
+        private void btnLaunchRealm_Click(object sender, EventArgs e)
+        {
+            int properRealm = checkForRealms();
+            if (properRealm != -1)
+            {
+                if (writeRealmFile(properRealm))
+                {
+                    try
+                    {
+                        Process.Start(clientTable.Rows[properRealm][2].ToString());
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Could not launch client. Check configuration for clients matching path:" + Environment.NewLine + clientTable.Rows[realmRowSelected][2].ToString());
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Could not launch client. Check configuration for clients matching version " + realmTable.Rows[realmRowSelected][2].ToString());
+            }
+        }
+
+        private bool writeRealmFile(int properRealm)
+        {
+            bool isDone = false;
+            string wtfPath = Path.GetDirectoryName(clientTable.Rows[properRealm][2].ToString());
+            string realmFile = wtfPath + "\\realmlist.wtf";
+            try
+            {
+                File.WriteAllText(realmFile, string.Empty);
+                using (FileStream fs = new FileStream(realmFile, FileMode.OpenOrCreate))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        sw.WriteLine("set realmlist " + realmTable.Rows[realmRowSelected][4].ToString());
+                        sw.WriteLine("set pathchlist " + realmTable.Rows[realmRowSelected][5].ToString());
+                        isDone = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Could not update realm list in " + Environment.NewLine + wtfPath + Environment.NewLine + "Check to make sure directory exists & repair realm file if needed");
+            }
+            return isDone;
+        }
+
+        private int checkForRealms()
+        {
+            int i = -1;
+            string clientNeeded = realmTable.Rows[realmRowSelected][2].ToString();
+            foreach (DataRow r in clientTable.Rows)
+            {
+                if (r[1].ToString() == clientNeeded)
+                {
+                    i = clientTable.Rows.IndexOf(r);
+                }
+            }
+            return i;
+        }
+    }
+}
